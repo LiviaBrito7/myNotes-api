ac9adaa50be19030cebb354c5897aa3adae243b5
Lívia Brito <analiviavicenar@gmail.com>
2024-10-01 20:49:46 -0300

    adicionando middleware em outras rotas

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const { createUserController, getAllUsersController, updateUserController, deleteUserController, getUserController, loginUserController  } = require('../controllers/userController');
-const authMiddleware = require('../middleware/AuthMiddleware');
-const router = express.Router();
+const {  createUserController, getAllUsersController, updateUserController, deleteUserController, getUserController, loginUserController } = require('../controllers/userController');
 
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
 router.post('/users', createUserController);
 router.post('/login', loginUserController);
 router.get('/users/me', authMiddleware, (req, res) => {
-  res.json(req.user); 
+  res.json(req.user);
 });
-router.get('/users', getAllUsersController);
-router.get('/users/:id', getUserController);
-router.put('/users/:id', updateUserController);  
-router.delete('/users/:id', deleteUserController); 
+router.get('/users', authMiddleware, getAllUsersController);
+router.get('/users/:id', authMiddleware, getUserController);
+router.put('/users/:id', authMiddleware, updateUserController);
+router.delete('/users/:id', authMiddleware, deleteUserController);
 
 module.exports = router;
