68dbb2f12bf4b1405e4910a8e39761d495d39b1b
Lívia Brito <analiviavicenar@gmail.com>
2024-10-05 02:28:11 -0300

    adicionando cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
-const app = express();
+const cors = require('cors')
+const app = express()
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 app.use(userRoutes);
 app.use(noteRoutes);
