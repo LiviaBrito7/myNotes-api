84030f854ed92f90af6893c1a899be25196a4adf
Lívia Brito <analiviavicenar@gmail.com>
2024-10-19 16:23:17 -0300

    usando sql puro para fazer query

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -6,18 +6,8 @@ async function createNote(data) {
   return prisma.note.create({ data });
 }
 
-async function getAllNotes() {
-  return prisma.note.findMany({ 
-    include: {
-    user: {
-      select: {
-        id: true,
-        name: true,
-        email: true,
-      }
-    }
-  }
-});
+async function getAllNotes(id) {
+  return await prisma.$queryRaw`select * from note where userId = ${id}`
 }
 
 async function getNote(id) {
